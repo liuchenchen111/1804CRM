136fea2db6aa4d1b50999fa8d33e88a21f5ee09a
unknown <1987836281@zhang.com>
2018-10-31 10:21:20 +0800

    路由

--- CRM/routes/web.php
+++ CRM/routes/web.php
@@ -22,3 +22,17 @@ Route::any('client_add','Admin\ClientController@client_add');
 
 /*新增客户操作*/
 Route::any('client_add_db','Admin\ClientController@client_add_db');
+
+/*根据pid 获取地址信息*/
+Route::any('get_region','Admin\ClientController@get_region');
+
+/*
+ * 客户删除
+ */
+ Route::any('client_del','Admin\ClientController@client_del');
+
+/*
+* 客户修改
+*/
+ Route::any('client_save','Admin\ClientController@client_save');
+ Route::any('client_save_db','Admin\ClientController@client_save_db');
